55cea5a9e4296b9eecee3cb00a5cbd291b6fe926
TeaLeafMedia <tealeafmedia@proton.me>
2026-04-21 11:14:38 -0700

    Test 1. created for correct letter guesses.

--- test_guess_the_word.py
+++ test_guess_the_word.py
@@ -5,3 +5,12 @@
 # Import function from our game file
 from guess_the_word import get_word
 
+
+# Test 1 checking for proper display of correctly
+# guessed letters.
+def tests():
+    result = get_word("pasta", ["p"])
+    if result == "p _ _ _ _":
+        print("Letters are showing as expected.")
+    else:
+        print("Test failed.")
